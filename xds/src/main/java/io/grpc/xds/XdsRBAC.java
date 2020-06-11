74383f3decb6aa19eda6d2e04338883b4abcdb7b
Cindy Xue <cindyxue@google.com>
2020-06-11 14:52:45 -0700

    Tried using the imported RBAC object

---
+++ xds/src/main/java/io/grpc/xds/XdsRBAC.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+import io.envoyproxy.envoy.config.rbac.v2.RBAC;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+
+/**
+ * An {@link XdsClient} instance encapsulates all of the logic for communicating with the xDS
+ * server. It may create multiple RPC streams (or a single ADS stream) for a series of xDS
+ * protocols (e.g., LDS, RDS, VHDS, CDS and EDS) over a single channel. Watch-based interfaces
+ * are provided for each set of data needed by gRPC.
+ */
+class XdsRBAC {
+    private RBAC rbac;
+
+    public XdsRBAC(@Nullable RBAC rbac) {
+        this.rbac = rbac;
+    }
+
+    @Nullable
+    RBAC getRbac() {
+        return rbac;
+    }
+}
