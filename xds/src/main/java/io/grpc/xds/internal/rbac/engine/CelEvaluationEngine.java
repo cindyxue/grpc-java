92fb045a012e8f6ed96a05b3d944f26f74fb5c2a
Cindy Xue <cindyxue@google.com>
2020-06-25 13:47:40 -0700

    Converted Expr into ParsedExpr

--- xds/src/main/java/io/grpc/xds/internal/rbac/engine/CelEvaluationEngine.java
+++ xds/src/main/java/io/grpc/xds/internal/rbac/engine/CelEvaluationEngine.java
@@ -18,6 +18,10 @@ package io.grpc.xds.internal;
 
 import com.google.api.expr.v1alpha1.CheckedExpr;
 import com.google.api.expr.v1alpha1.Expr;
+import com.google.api.expr.v1alpha1.ParsedExpr;
+import com.google.api.expr.v1alpha1.SourceInfo;
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.Descriptors.Descriptor;
 import io.envoyproxy.envoy.config.rbac.v2.Policy;
 import io.envoyproxy.envoy.config.rbac.v2.RBAC;
 import io.grpc.Attributes;
@@ -38,78 +42,21 @@ import java.util.Map;
  * Envoy RBAC condition in gRPC-Java.
  */
 public class CelEvaluationEngine<ReqT, RespT> {
-  private enum Action {
-    ALLOW,
-    DENY
-  }
-
-  /** Holds conditions of RBAC policies. */
-  private class Condition {
-    private Expr expr;
-
-    public Condition(Expr expr) {
-      this.expr = expr;
-    }
-
-    /**
-     * Check if arg matches with the condition expr
-     * @param args EvaluateArgs that is used to evaluate the conditions.
-     * @return whether or not arg matches this expr
-     */
-    public boolean matches(EvaluateArgs<ReqT, RespT> args) {
-      // Extract Envoy Attributes from EvaluateArgs
-      ServerCall<ReqT, RespT> call = args.getCall();
-      String requestUrlPath = "";
-      String requestHost = call.getMethodDescriptor().getServiceName();
-      String requestMethod = call.getMethodDescriptor().getFullMethodName();
-      String sourceAddress = call.getAttributes()
-        .get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
-      int sourcePort = 0;
-      String destinationAddress = call.getAttributes()
-        .get(Grpc.TRANSPORT_ATTR_LOCAL_ADDR).toString();
-      int destinationPort = 0;
-      Metadata requestHeaders = args.getHeaders();
-      String connectionRequestedServerName = call.getAuthority();
-      String connectionUriSanPeerCertificate = "";
-
-      // List<Descriptor> descriptors = new ArrayList<>();
-      // RuntimeTypeProvider messageProvider = DescriptorMessageProvider.dynamicMessages(descriptors);
-      // Dispatcher dispatcher = DefaultDispatcher.create();
-      // Interpreter interpreter = new DefaultInterpreter(messageProvider, dispatcher);
-  
-      // CheckedExpr checkedResult = CheckedExpr.newBuilder().build();
-  
-      // Map<String, Object> map = new HashMap<>();
-      // map.put("requestUrlPath", new Object());
-      // map.put("requestHost", new Object());
-      // map.put("requestMethod", new Object());
-      // map.put("requestHeaders", new Object());
-      // map.put("sourceAddress", new Object());
-      // map.put("sourcePort", new Object());
-      // map.put("destinationAddress", new Object());
-  
-      // ImmutableMap<String, Object> apiAttributes = ImmutableMap.copyOf(map);
-  
-      // Activation activation = Activation.copyOf(apiAttributes);
-      // Object result = interpreter.createInterpretable(checkedResult).eval(activation);
-      
-      // Todo
-      return true;
-    }
-  }
-
-  Action action;
-  Map<String, Condition> conditions;
+  private RBAC.Action action;
+  private Map<String, Expr> conditions;
+  private Map<String, Object> attributes;
 
   /**
    * Builds a CEL evaluation engine from Envoy RBAC.
    * @param rbac input envoy RBAC policies.
    */
   public CelEvaluationEngine(@Nullable RBAC rbac) {
-    this.action = rbac.getAction() == RBAC.Action.ALLOW ? Action.ALLOW : Action.DENY;
+    this.action = rbac.getAction();
     this.conditions = new HashMap<>();
+    this.attributes = new HashMap<>();
+
     for(Map.Entry<String, Policy> entry: rbac.getPolicies().entrySet()) {
-        this.conditions.put(entry.getKey(), new Condition(entry.getValue().getCondition()));
+        this.conditions.put(entry.getKey(), entry.getValue().getCondition());
     }
   }
 
@@ -123,11 +70,12 @@ public class CelEvaluationEngine<ReqT, RespT> {
    * @param args EvaluateArgs that is used to evaluate the conditions.
    * @return an AuthorizationDecision.
    */
-  public AuthorizationDecision evaluate(EvaluateArgs<ReqT, RespT> args) {
+  public AuthorizationDecision evaluate(EvaluateArgs<ReqT, RespT> args) 
+    throws InterpreterException {
     AuthorizationDecision authDecision = new AuthorizationDecision();
-    for (Map.Entry<String, Condition> entry : this.conditions.entrySet()) {
-      if (entry.getValue().matches(args)) {
-        authDecision.decision = this.action == Action.ALLOW ? 
+    for (Map.Entry<String, Expr> entry : this.conditions.entrySet()) {
+      if (CelEvaluationEngine.matches(entry.getValue(), args)) {
+        authDecision.decision = this.action == RBAC.Action.ALLOW ? 
             AuthorizationDecision.Decision.ALLOW : AuthorizationDecision.Decision.DENY;
         authDecision.authorizationContext = "policy matched: " + entry.getKey();
         return authDecision;
@@ -137,5 +85,131 @@ public class CelEvaluationEngine<ReqT, RespT> {
     authDecision.decision = AuthorizationDecision.Decision.UNKNOWN;
     authDecision.authorizationContext = "Unable to decide based on given information - no policies matched";
     return authDecision;
+  }
+
+  /**
+   * Determines if a condition matches the given EvaluateArgs using Cel library
+   * @param <ReqT>
+   * @param <RespT>
+   * @param conditions
+   * @param args
+   * @return
+   * @throws InterpreterException
+   */
+  private static <ReqT, RespT> boolean matches(Expr conditions, EvaluateArgs<ReqT, RespT> args) 
+    throws InterpreterException {
+    CheckedExpr checkedResult = CelEvaluationEngine.parseConditions(conditions);
+    ImmutableMap<String, Object> apiAttributes = CelEvaluationEngine.extractFields(args);
+    Object result = CelEvaluationEngine.celEvaluate(checkedResult, apiAttributes);
+
+    if(result instanceof Boolean) {
+      return Boolean.parseBoolean(result.toString());
+    }
+
+    return false;
+  }
+
+  /**
+   * Helper function for cel evaluate.
+   * @param <ReqT>
+   * @param <RespT>
+   * @param checkedResult
+   * @param apiAttributes
+   * @return
+   * @throws InterpreterException
+   */
+  private static <ReqT, RespT> Object celEvaluate(CheckedExpr checkedResult, 
+    ImmutableMap<String, Object> apiAttributes) throws InterpreterException {
+    List<Descriptor> descriptors = new ArrayList<>();
+    RuntimeTypeProvider messageProvider = DescriptorMessageProvider.dynamicMessages(descriptors);
+    Dispatcher dispatcher = DefaultDispatcher.create();
+    Interpreter interpreter = new DefaultInterpreter(messageProvider, dispatcher);
+
+    Activation activation = Activation.copyOf(apiAttributes);
+    Object result = interpreter.createInterpretable(checkedResult).eval(activation);
+
+    return result;
+  }
+
+  /**
+   * Convert conditions from Expr type into CheckedExpr type
+   * @param conditions
+   * @return
+   */
+  private static CheckedExpr parseConditions(Expr conditions) {
+    if(conditions == null) {
+      return null;
+    }
+
+    ParsedExpr parsedConditions = ParsedExpr.newBuilder()
+    .setExpr(conditions)
+    .setSourceInfo(SourceInfo.newBuilder().build())
+    .build();
+
+    return CheckedExpr.newBuilder().build();
+  }
+
+  /**
+   * Extract Envoy Attributes from EvaluateArgs
+   * @param args
+   * @return 
+   */
+  private static <ReqT, RespT> ImmutableMap<String, Object> extractFields(
+    EvaluateArgs<ReqT, RespT> args) {
+    if(args == null) {
+      return null;
+    }
+
+    ServerCall<ReqT, RespT> call = args.getCall();
+    String requestUrlPath = "";
+    String requestHost = call.getMethodDescriptor().getServiceName();
+    String requestMethod = call.getMethodDescriptor().getFullMethodName();
+    Metadata requestHeaders = args.getHeaders();
+    String sourceAddress = call.getAttributes()
+      .get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
+    int sourcePort = 0;
+    String destinationAddress = call.getAttributes()
+      .get(Grpc.TRANSPORT_ATTR_LOCAL_ADDR).toString();
+    int destinationPort = 0;
+    String connectionRequestedServerName = call.getAuthority();
+    String connectionUriSanPeerCertificate = "";
+
+    Map<String, Object> map = new HashMap<>();
+    // map.put("requestUrlPath", requestUrlPath);
+    map.put("requestHost", requestHost);
+    map.put("requestMethod", requestMethod);
+    map.put("requestHeaders", requestHeaders);
+    map.put("sourceAddress", sourceAddress);
+    // map.put("sourcePort", sourcePort);
+    map.put("destinationAddress", destinationAddress);
+    // map.put("destinationPort", destinationPort);
+    map.put("connectionRequestedServerName", connectionRequestedServerName);
+    // map.put("connectionUriSanPeerCertificate", connectionUriSanPeerCertificate);
+
+    return ImmutableMap.copyOf(map);
+  }
+
+  private ImmutableMap<String, Object> extractAttributes(EvaluateArgs<ReqT, RespT> args) {
+    ServerCall<ReqT, RespT> call = args.getCall();
+    setRequestHost(call);
+    setRequestMethod(call);
+
+    return ImmutableMap.copyOf(this.attributes);
+  }
+
+  private void setRequestHost(ServerCall<ReqT, RespT> call) {
+    String requestHost = call.getMethodDescriptor().getServiceName();
+    if(requestHost == null || requestHost.length() == 0) {
+      return;
+    }
+    this.attributes.put("requestHost", requestHost);
+  }
+
+  private void setRequestMethod(ServerCall<ReqT, RespT> call) {
+    String requestMethod = call.getMethodDescriptor().getFullMethodName();
+    if(requestMethod == null || requestMethod.length() == 0) {
+      return;
+    }
+    this.attributes.put("requestMethod", requestMethod);
   }
 }
