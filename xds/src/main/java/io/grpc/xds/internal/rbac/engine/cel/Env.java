1cb4f085026df88554ee4b5a7d84a68cdb331391
Cindy Xue <cindyxue@google.com>
2020-06-30 13:09:08 -0700

    Added Env, Errors, TypeProvider

--- xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Env.java
+++ xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Env.java
@@ -16,6 +16,88 @@
 
 package io.grpc.xds.internal;
 
+import com.google.api.expr.v1alpha1.Type;
+import com.google.api.expr.v1alpha1.Decl;
+import com.google.api.expr.v1alpha1.Decl.IdentDecl;
+import com.google.api.tools.expr.features.ExprFeatures;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Environment used during checking of expressions.
+ * Provides name resolution and error reporting.
+ */
 public class Env {
-  
+  /** Type provider responsible for resolving CEL message references to strong types. */
+  private final TypeProvider typeProvider;
+
+  /**
+   * Stack of declaration groups where each entry in stack represents a scope capable of hinding
+   * declarations lower in the stack.
+   */
+  private final Stack<DeclGroup> decls = new Stack<>();
+
+  /** Object used for error reporting. */
+  private final Errors errors;
+
+  /** CEL Feature flags. */
+  private final ImmutableSet<ExprFeatures> exprFeatures;
+
+  private Env(
+      Errors errors,
+      TypeProvider typeProvider,
+      DeclGroup declGroup,
+      ImmutableSet<ExprFeatures> exprFeatures) {
+    this.exprFeatures = exprFeatures;
+    this.errors = Preconditions.checkNotNull(errors);
+    this.typeProvider = Preconditions.checkNotNull(typeProvider);
+    this.decls.push(Preconditions.checkNotNull(declGroup));
+  }
+
+  /**
+   * Adds a declaration to the environment, based on the Decl proto. Will report errors if the
+   * declaration overlaps with an existing one, or clashes with a macro.
+   */
+  public Env add(Decl decl) {
+    return this;
+  }
+
+  /** Adds simple name declaration to the environment for a non-function. */
+  public Env add(String name, Type type) {
+    return add(Decl.newBuilder().build());
+  }
+
+  /**
+   * Creates an {@code Env} value configured with the standard types, functions, and operators,
+   * configured with a custom {@code typeProvider}.
+   *
+   * <p>Note: standard declarations are configured in an isolated scope, and may be shadowed by
+   * subsequent declarations with the same signature.
+   */
+  public static Env standard(
+      Errors errors, TypeProvider typeProvider, ExprFeatures... exprFeatures) {
+    return standard(errors, typeProvider, ImmutableSet.copyOf(exprFeatures));
+  }
+
+  /**
+   * Creates an {@code Env} value configured with the standard types, functions, and operators,
+   * configured with a custom {@code typeProvider} and a reference to the set of {@code
+   * exprFeatures} enabled in the environment.
+   *
+   * <p>Note: standard declarations are configured in an isolated scope, and may be shadowed by
+   * subsequent declarations with the same signature.
+   */
+  public static Env standard(
+      Errors errors, TypeProvider typeProvider, ImmutableSet<ExprFeatures> exprFeatures) {
+    return new Env(errors, typeProvider, new DeclGroup(), exprFeatures);
+  }
+
+  /** Object for managing a group of declarations within a scope. */
+  public static class DeclGroup {
+  }
 }
---
+++ xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Main.java
@@ -0,0 +1,55 @@
+package io.grpc.xds.internal;
+
+import com.google.api.expr.v1alpha1.CheckedExpr;
+import com.google.api.expr.v1alpha1.Expr;
+import com.google.api.expr.v1alpha1.ParsedExpr;
+import com.google.api.expr.v1alpha1.SourceInfo;
+import com.google.api.expr.v1alpha1.Type;
+import com.google.api.tools.expr.features.ExprFeatures;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.Descriptors.Descriptor;
+import io.grpc.Metadata;
+import io.grpc.xds.InterpreterException;
+import java.lang.RuntimeException;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+  public static void main(String[] args) throws InterpreterException {
+    Errors errors = new Errors("source_location", null);
+    TypeProvider typeProvider = new DescriptorTypeProvider();
+    Env env = Env.standard(errors, typeProvider);
+    env.add("requestUrlPath", Type.newBuilder().build(), null);
+    env.add("requestHost", Type.newBuilder().build(), null);
+    env.add("requestMethod", Type.newBuilder().build(), null);
+
+    List<Descriptor> descriptors = new ArrayList<>();
+    RuntimeTypeProvider messageProvider = DescriptorMessageProvider.dynamicMessages(descriptors);
+    Dispatcher dispatcher = DefaultDispatcher.create();
+    Interpreter interpreter = new DefaultInterpreter(messageProvider, dispatcher);
+
+    Expr conditions = Expr.newBuilder().build();
+    ParsedExpr parsedConditions = ParsedExpr.newBuilder()
+      .setExpr(conditions)
+      .setSourceInfo(SourceInfo.newBuilder().build())
+      .build();
+    CheckedExpr checkedResult = ExprChecker.check(env, "", parsedConditions);
+
+    if (errors.getErrorCount() > 0) {
+      throw new RuntimeException(errors.getAllErrorsAsString());
+    }    
+
+    Map<String, Object> map = new HashMap<>();
+    map.put("requestUrlPath", new Object());
+    map.put("requestHost", new Object());
+    map.put("requestMethod", new Object());
+    ImmutableMap<String, Object> apiAttributes = ImmutableMap.copyOf(map);
+
+    Activation activation = Activation.copyOf(apiAttributes);
+    Object result = interpreter.createInterpretable(checkedResult).eval(activation);
+  }
+}
