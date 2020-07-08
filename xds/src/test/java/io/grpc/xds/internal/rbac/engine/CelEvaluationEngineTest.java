f607d76ad79f5c9954a5979e840b32f9044e7b47
Cindy Xue <cindyxue@google.com>
2020-07-08 14:00:14 -0700

    Add unit test for cel engine ctor

--- xds/src/test/java/io/grpc/xds/internal/rbac/engine/CelEvaluationEngineTest.java
+++ xds/src/test/java/io/grpc/xds/internal/rbac/engine/CelEvaluationEngineTest.java
@@ -18,19 +18,13 @@ package io.grpc.xds.internal;
 
 import static com.google.common.truth.Truth.assertThat;
 
-import com.google.api.expr.v1alpha1.Expr;
-import com.google.common.collect.ImmutableMap;
-import com.google.protobuf.Descriptors.Descriptor;
 import io.envoyproxy.envoy.config.rbac.v2.RBAC;
-import io.grpc.xds.InterpreterException;
-import java.lang.String;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import org.junit.Rule;
-import org.junit.rules.ExpectedException;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -38,41 +32,99 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class CelEvaluationEngineTest<ReqT, RespT> {
   @Rule
-  public ExpectedException thrown = ExpectedException.none();
+  public final ExpectedException thrown = ExpectedException.none();
+  private CelEvaluationEngine<ReqT,RespT> engine;
 
-  @Test(expected=IllegalArgumentException.class)
-  public void testEmptyRbacList() {
-    List<RBAC> rbacList = new ArrayList<>();
-    CelEvaluationEngine<ReqT,RespT> engine = new CelEvaluationEngine<>(rbacList);
-    thrown.expect(IllegalArgumentException.class);
-    thrown.expectMessage("Invalid RBAC list size, must provide either one RBAC or two RBACs. ");
+  @Test
+  public void createEngineSingleRbac() {
+    RBAC rbac = RBAC.newBuilder().build();
+    List<RBAC> rbacList = new ArrayList<>(Arrays.asList(new RBAC[] {rbac}));
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNotNull();
   }
 
-  // @Test
-  // public void testSingleRbac() {
-  //   RBAC rbac = RBAC.newBuilder().build();
-  //   List<RBAC> rbacList = new ArrayList<>(rbac);
-  //   try {
-  //     CelEvaluationEngine engine = new CelEvaluationEngine(rbacList);
-  //   } catch (IllegalArgumentException e) {
-  //     assertThat(e).isNull();
-  //   }
-  // }
+  @Test
+  public void createEngineRbacPairOfDenyAllow() {
+    RBAC rbac1 = RBAC.newBuilder()
+        .setAction(RBAC.Action.DENY)
+        .build();
+    RBAC rbac2 = RBAC.newBuilder()
+        .setAction(RBAC.Action.ALLOW)
+        .build();
+    List<RBAC> rbacList = new ArrayList<>(Arrays.asList(new RBAC[] {rbac1, rbac2}));
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNotNull();
+  }
 
-  // @Test
-  // public void testRbacPairWithCorrectActions() {
-  //   RBAC rbac1 = RBAC.newBuilder().build();
-  //   RBAC rbac2 = RBAC.newBuilder().build();
-  // }
-  
-  // @Test
-  // public void testRbacPairWithWrongActions() {
-  //   RBAC rbac1 = RBAC.newBuilder().build();
-  //   RBAC rbac2 = RBAC.newBuilder().build();
-  // }
+  @Test
+  public void failToCreateEngineIfRbacPairOfAllowAllow() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage( "Invalid RBAC list, " 
+        + "must provide a RBAC with DENY action followed by a RBAC with ALLOW action. ");
+    RBAC rbac1 = RBAC.newBuilder()
+        .setAction(RBAC.Action.ALLOW)
+        .build();
+    RBAC rbac2 = RBAC.newBuilder()
+        .setAction(RBAC.Action.ALLOW)
+        .build();
+    List<RBAC> rbacList = new ArrayList<>(Arrays.asList(new RBAC[] {rbac1, rbac2}));
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNull();
+  }
 
-  // @Test
-  // public void testInvalidRbacList() throws IllegalArgumentException {
+  @Test
+  public void failToCreateEngineIfRbacPairOfAllowDeny() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage( "Invalid RBAC list, " 
+        + "must provide a RBAC with DENY action followed by a RBAC with ALLOW action. ");
+    RBAC rbac1 = RBAC.newBuilder()
+        .setAction(RBAC.Action.ALLOW)
+        .build();
+    RBAC rbac2 = RBAC.newBuilder()
+        .setAction(RBAC.Action.DENY)
+        .build();
+    List<RBAC> rbacList = new ArrayList<>(Arrays.asList(new RBAC[] {rbac1, rbac2}));
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNull();
+  }
 
-  // }
+  @Test
+  public void failToCreateEngineIfRbacPairOfDenyDeny() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage( "Invalid RBAC list, " 
+        + "must provide a RBAC with DENY action followed by a RBAC with ALLOW action. ");
+    RBAC rbac1 = RBAC.newBuilder()
+        .setAction(RBAC.Action.DENY)
+        .build();
+    RBAC rbac2 = RBAC.newBuilder()
+        .setAction(RBAC.Action.DENY)
+        .build();
+    List<RBAC> rbacList = new ArrayList<>(Arrays.asList(new RBAC[] {rbac1, rbac2}));
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNull();
+  }
+
+  @Test
+  public void failToCreateEngineIfEmptyRbacList() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(
+        "Invalid RBAC list size, must provide either one RBAC or two RBACs. ");
+    List<RBAC> rbacList = new ArrayList<>();
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNull();
+  }
+
+  @Test
+  public void failToCreateEngineIfTooLongRbacList() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(
+        "Invalid RBAC list size, must provide either one RBAC or two RBACs. ");
+    List<RBAC> rbacList = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      RBAC rbac = RBAC.newBuilder().build();
+      rbacList.add(rbac);
+    }
+    engine = new CelEvaluationEngine<>(rbacList);
+    assertThat(engine).isNull();
+  }
 }
